1c198f4c50f77d27e0877d04d8d8c01d91e635dc
15138730370 <1037483576@qq.com>
2018-01-15 11:44:10 +0800

    save to back

--- src/app/runner-map/runner-map.ts
+++ src/app/runner-map/runner-map.ts
@@ -43,7 +43,7 @@ export class RunnerMapComponent {
         this.form = this.fb.group({
             start: [{}],
             end: [{}],
-            time: [{}],
+            timeval: [{}],
             weight: ['']
         });
         this.on((res: any) => {
